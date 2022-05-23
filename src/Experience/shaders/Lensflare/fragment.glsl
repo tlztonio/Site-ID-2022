8de012fab147e165a8d5e020304cfbdc04f5ff7c
tlztonio <antoine.galy.lamon@gmail.com>
2022-05-24 01:46:37 +0200

    update post shader

--- src/Experience/shaders/Lensflare/fragment.glsl
+++ src/Experience/shaders/Lensflare/fragment.glsl
@@ -28,6 +28,12 @@ void main()
 {
     // for the positions I use a scale were y = 1 and x = 1.something to modify the uvs and make circular shapes on any res
 
+    // TODO:
+    // rayons soleil image 
+    // rayons soleils deviennent plus intenses avec une distance plus courte 
+    // rgb shift
+    // small random rays
+
 	vec4 color = texture2D(tDiffuse, vUv); // classic uv
 
     vec2 newvUv = vec2(vUv.x * uAspectRatio, vUv.y);
@@ -38,24 +44,20 @@ void main()
     float spacing = flareLength * 0.25;
     float brightness = (1.0 - (flareLength * 0.4));
 
-    vec2 flarePos1 = calcFlaresPos(spacing, 1.0, sunToCenter);
-    vec2 flarePos2 = calcFlaresPos(spacing, 1.0, sunToCenter);
-    vec2 flarePos3 = calcFlaresPos(spacing, 2.0, sunToCenter);
-    vec2 flarePos4 = calcFlaresPos(spacing, 3.0, sunToCenter);
+    vec2 flarePos1 = calcFlaresPos(spacing, 0.5, sunToCenter);
+    vec2 flarePos2 = calcFlaresPos(spacing, 1.4, sunToCenter);
+    vec2 flarePos3 = calcFlaresPos(spacing, 2.25, sunToCenter);
+    // vec2 flarePos4 = calcFlaresPos(spacing, 4.0, sunToCenter);
 
     // vec4 flare1 = texture2D(tLensFlare1, flarePos1);
-    // vec4 flare2 = texture2D(tLensFlare2, flarePos2);
-    // vec4 flare3 = texture2D(tLensFlare2, flarePos3);
-    // vec4 flare4 = texture2D(tLensFlare3, flarePos4);
 
-
-    float flare1 = 1.0 - clamp( distance(newvUv, flarePos1) * 20.0, 0.9, 1.0);
-    float flare2 = 1.0 - clamp( distance(newvUv, flarePos2) * 20.0, 0.9, 1.0);
+    // distance * size, whiteness clamp
+    float flare1 = 1.0 - clamp( distance(newvUv, flarePos1) * 35.0, 0.9, 1.0);
+    float flare2 = 1.0 - clamp( distance(newvUv, flarePos2) * 30.0, 0.9, 1.0);
     float flare3 = 1.0 - clamp( distance(newvUv, flarePos3) * 20.0, 0.9, 1.0);
-    float flare4 = 1.0 - clamp( distance(newvUv, flarePos4) * 20.0, 0.9, 1.0);
+    // float flare4 = 1.0 - clamp( distance(newvUv, flarePos4) * 20.0, 0.9, 1.0);
 
-    // vec4 flares = flare1 + flare2 ;
-    vec4 flares = vec4(flare1 + flare2 + flare3 + flare4);
+    vec4 flares = vec4(flare1 + flare2 + flare3) * max( 0.4-flareLength*0.2, 0.0);
 
-    gl_FragColor = color + flares*0.1;
+    gl_FragColor = color + flares;
 }
